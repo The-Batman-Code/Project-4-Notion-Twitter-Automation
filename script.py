84ee01fa9d577e41704daa0d51cb28d694b16f25
Kartik Saxena <mailtokartiksaxena@gmail.com>
2023-01-27 21:01:48 +0530

    Update script.py

--- script.py
+++ script.py
@@ -16,10 +16,10 @@ def get_time():
 
 
 def tweet(data):
-    consumer_key = "VUF4ZI0xakf2GiL8miX9eQKab"
-    consumer_secret = "WV6PvML0wi2IyvgSTzNHirTdtkC2qGFmxeM550vGCF3v2JpqTp"
-    access_token = "1582044525140545536-1z3T6Zcx7qVngToIx40CtvxOmk6BUF"
-    access_token_secret = "Ynp3AH2f003MGyYQ9FGiyZB2xk1JZKX6KUNOQTh1PoRbn"
+    consumer_key = "Enter here"
+    consumer_secret = "Enter here"
+    access_token = "Enter here"
+    access_token_secret = "Enter here"
 
     # authentication of consumer key and secret
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -34,15 +34,15 @@ def tweet(data):
 
     # time.sleep(600)
 while True:
-    if get_time() > "10:00:00" and get_time() < "10:00:02":
+    if get_time() > "10:00:00" and get_time() < "10:05:00":
         print(data[0])
         tweet(data[0])
         break
-    elif get_time() > "14:00:00" and get_time() < "14:00:02":
+    elif get_time() > "14:00:00" and get_time() < "14:05:00":
         print(data[1])
         tweet(data[1])
         break
-    elif get_time() > "18:00:00" and get_time() < "18:00:02":
+    elif get_time() > "18:00:00" and get_time() < "18:05:00":
         print(data[2])
         tweet(data[2])
         break
